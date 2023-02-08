6e56b0c5d14843a18abbe3d082c154c8090a0e45
HatterJS <hatter3ds@gmail.com>
2023-02-08 14:23:00 +0200

    backend authorization

--- server/server.js
+++ server/server.js
@@ -25,21 +25,63 @@ app.post('/authorization/registration', registrationValidation, async (req, res)
 
     const password = req.body.password;
     const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
+    const hash = await bcrypt.hash(password, salt);
 
     const doc = new UserModel({
       email: req.body.email,
-      passwordHash,
+      passwordHash: hash,
       fullName: req.body.fullName,
       avatarUrl: req.body.avatarUrl
     });
 
     const user = await doc.save();
 
-    res.json(user);
+    const token = jwt.sign(
+      {
+        _id: user._id
+      },
+      'blog_secret_key',
+      {
+        expiresIn: '30d'
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: 'Реєстрація пройшла не коректно' });
+  }
+});
+
+app.post('/authorization/authorization', async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({ message: 'Користувача з таким email або паролем не існує' });
+    }
+
+    const isValidPassword = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+    if (!isValidPassword) {
+      return res.status(404).json({ message: 'Користувача з таким email або паролем не існує' });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id
+      },
+      'blog_secret_key',
+      {
+        expiresIn: '30d'
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+    res.json({ ...userData, token });
+  } catch (err) {
+    res.status(500).json('Авторизація пройшла не коректно');
   }
 });
 
