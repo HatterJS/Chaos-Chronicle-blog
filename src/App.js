1cb55b2394dce070934fe00b2462d5b3eef06f28
HatterJS <hatter3ds@gmail.com>
2023-02-13 14:10:05 +0200

    initial markup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <div className="header__logo">
+          <img src="img/logo/dub.jpg" alt="logo" />
+        </div>
+        <div className="header__liks">
+          <div className="header__home">Головна</div>
+          <div className="header__about">Про блог</div>
+          <div className="header__contacts">Контакти</div>
+        </div>
+        <div className="header__user">
+          <div className="header__authorization">Вхід</div>
+          <div className="header__registration">Реєстрація</div>
+        </div>
+      </header>
+      <main>
+        <div className="sign"></div>
+        <nav>
+          <ul>
+            <li>Нові</li>
+            <li>Популярні</li>
+            <li>Автори</li>
+          </ul>
+          <div className="nav__search">
+            <input type="search" />
+          </div>
+        </nav>
+        <div className="articles">
+          <div className="article">
+            <div className="article__image">
+              <img
+                src="img/article/Welcome-to-Ukraine-by-Stanislav-Lunin-scaled.jpg"
+                alt="article-img"
+              />
+            </div>
+            <div className="article__description">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi consequuntur dicta
+              excepturi fuga, nihil pariatur accusamus possimus maxime libero non earum commodi sint
+              ducimus, amet tenetur obcaecati odio quae vel!
+            </div>
+          </div>
+        </div>
+        <aside>
+          <div className="aside__tags">
+            <p>Популярні теги</p>
+            <ul>
+              <li>Україна</li>
+              <li>перемога</li>
+              <li>робота</li>
+            </ul>
+          </div>
+          <div className="aside__comments">
+            <p>Останні коментарі</p>
+            <div className="comment">
+              <div className="comment__avatar">
+                <img src="img/avatars/01.png" alt="avatar" />
+              </div>
+              <div className="comment__content">
+                <div className="comment__userName">Василь Черешня</div>
+                <div className="comment__text">
+                  Мені дуже сподобався цей блог. Буду продовжувати читати.
+                </div>
+              </div>
+            </div>
+          </div>
+        </aside>
+      </main>
+      <footer>
+        <div className="footer__logo"></div>
+        <div className="footer__social"></div>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
